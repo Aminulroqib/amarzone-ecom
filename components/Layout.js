44117d54b38fc0b093d7062777e1b2f2fb7aeca6
robotrybd <robotrybd@gmail.com>
2021-12-29 21:08:59 +0600

    cart

--- components/Layout.js
+++ components/Layout.js
@@ -3,21 +3,23 @@ import Head from 'next/head';
 import NextLink from 'next/link';
 import {
   AppBar,
-  Container,
-  createTheme,
-  CssBaseline,
-  Link,
-  ThemeProvider,
   Toolbar,
   Typography,
+  Container,
+  Link,
+  ThemeProvider,
+  CssBaseline,
+  Switch,
+  Badge,
+  createTheme,
 } from '@mui/material';
 import useStyles from '../utils/styles';
 import { Store } from '../utils/Store';
 import Cookies from 'js-cookie';
 
-const Layout = ({ title, children, description }) => {
+export default function Layout({ title, description, children }) {
   const { state, dispatch } = useContext(Store);
-  const { darkMode } = state;
+  const { darkMode, cart } = state;
   const theme = createTheme({
     typography: {
       h1: {
@@ -30,37 +32,57 @@ const Layout = ({ title, children, description }) => {
         fontWeight: 400,
         margin: '1rem 0',
       },
-      palette: {
-        type: darkMode ? 'dark' : 'light',
-        primary: {
-          main: '#f0c000 !important',
-        },
-        secondary: {
-          main: '#208080',
-        },
+    },
+    palette: {
+      type: darkMode ? 'dark' : 'light',
+      primary: {
+        main: '#f0c000',
+      },
+      secondary: {
+        main: '#208080',
       },
     },
   });
   const classes = useStyles();
+  const darkModeChangeHandler = () => {
+    dispatch({ type: darkMode ? 'DARK_MODE_OFF' : 'DARK_MODE_ON' });
+    const newDarkMode = !darkMode;
+    Cookies.set('darkMode', newDarkMode ? 'ON' : 'OFF');
+  };
   return (
     <div>
-      {/* <Head>
-        <tittle>{title ? `${title} - Amarzone` : 'Amarzone'}</tittle>
+      <Head>
+        <title>{title ? `${title} - Next Amazona` : 'Next Amazona'}</title>
         {description && <meta name="description" content={description}></meta>}
-      </Head> */}
+      </Head>
       <ThemeProvider theme={theme}>
+        <CssBaseline />
         <AppBar position="static" className={classes.navbar}>
-          <CssBaseline />
           <Toolbar>
             <NextLink href="/" passHref>
               <Link>
-                <Typography className={classes.brand}>Amarzone</Typography>
+                <Typography className={classes.brand}>amazona</Typography>
               </Link>
             </NextLink>
             <div className={classes.grow}></div>
             <div>
+              <Switch
+                checked={darkMode}
+                onChange={darkModeChangeHandler}
+              ></Switch>
               <NextLink href="/cart" passHref>
-                <Link>Cart</Link>
+                <Link>
+                  {cart.cartItems.length > 0 ? (
+                    <Badge
+                      color="secondary"
+                      badgeContent={cart.cartItems.length}
+                    >
+                      Cart
+                    </Badge>
+                  ) : (
+                    'Cart'
+                  )}
+                </Link>
               </NextLink>
               <NextLink href="/login" passHref>
                 <Link>Login</Link>
@@ -70,11 +92,112 @@ const Layout = ({ title, children, description }) => {
         </AppBar>
         <Container className={classes.main}>{children}</Container>
         <footer className={classes.footer}>
-          <Typography>All Rights Reserved Amarzone.</Typography>
+          <Typography>All rights reserved. Next Amazona.</Typography>
         </footer>
       </ThemeProvider>
     </div>
   );
-};
+}
 
-export default Layout;
+// import React, { useContext } from 'react';
+// import Head from 'next/head';
+// import NextLink from 'next/link';
+// import {
+//   AppBar,
+//   Container,
+//   createTheme,
+//   CssBaseline,
+//   Switch,
+//   Link,
+//   ThemeProvider,
+//   Toolbar,
+//   Typography,
+//   Badge,
+// } from '@mui/material';
+// import useStyles from '../utils/styles';
+// import Store from '../utils/Store';
+// import Cookies from 'js-cookie';
+
+// const Layout = ({ title, children, description }) => {
+//   const { state, dispatch } = useContext(Store);
+//   const { darkMode, cart } = state;
+//   const theme = createTheme({
+//     typography: {
+//       h1: {
+//         fontSize: '1.6rem',
+//         fontWeight: 400,
+//         margin: '1rem 0',
+//       },
+//       h2: {
+//         fontSize: '1.4rem',
+//         fontWeight: 400,
+//         margin: '1rem 0',
+//       },
+//       palette: {
+//         type: darkMode ? 'dark' : 'light',
+//         primary: {
+//           main: '#f0c000',
+//         },
+//         secondary: {
+//           main: '#208080',
+//         },
+//       },
+//     },
+//   });
+//   const classes = useStyles();
+//   const darkModeChangeHandler = () => {
+//     dispatch({ type: darkMode ? 'DARK_MODE_OFF' : 'DARK_MODE_ON' });
+//     const newDarkMode = !darkMode;
+//     Cookies.set('darkMode', newDarkMode ? 'ON' : 'OFF');
+//   };
+//   return (
+//     <div>
+//       <Head>
+//         <title>{title ? `${title} - Amarzona` : 'Amarzona'}</title>
+//         {description && <meta name="description" content={description}></meta>}
+//       </Head>
+//       <ThemeProvider theme={theme}>
+//         <CssBaseline />
+//         <AppBar position="static" className={classes.navbar}>
+//           <Toolbar>
+//             <NextLink href="/" passHref>
+//               <Link>
+//                 <Typography className={classes.brand}>amazona</Typography>
+//               </Link>
+//             </NextLink>
+//             <div className={classes.grow}></div>
+//             <div>
+//               <Switch
+//                 checked={darkMode}
+//                 onChange={darkModeChangeHandler}
+//               ></Switch>
+//               <NextLink href="/cart" passHref>
+//                 <Link>
+//                   {cart.cartItems.length > 0 ? (
+//                     <Badge
+//                       color="secondary"
+//                       badgeContent={cart.cartItems.length}
+//                     >
+//                       Cart
+//                     </Badge>
+//                   ) : (
+//                     'Cart'
+//                   )}
+//                 </Link>
+//               </NextLink>
+//               <NextLink href="/login" passHref>
+//                 <Link>Login</Link>
+//               </NextLink>
+//             </div>
+//           </Toolbar>
+//         </AppBar>
+//         <Container className={classes.main}>{children}</Container>
+//         <footer className={classes.footer}>
+//           <Typography>All rights reserved. Next Amazona.</Typography>
+//         </footer>
+//       </ThemeProvider>
+//     </div>
+//   );
+// };
+
+// export default Layout;
--- pages/product/[slug].js
+++ pages/product/[slug].js
@@ -1,6 +1,6 @@
-import React from 'react';
-import Image from 'next/image';
+import React, { useContext } from 'react';
 import NextLink from 'next/link';
+import Image from 'next/image';
 import {
   Grid,
   Link,
@@ -12,25 +12,41 @@ import {
 } from '@mui/material';
 import Layout from '../../components/Layout';
 import useStyles from '../../utils/styles';
-import db from '../../utils/db';
 import Product from '../../models/Product';
+import db from '../../utils/db';
+import axios from 'axios';
+import { Store } from '../../utils/Store';
+import { useRouter } from 'next/router';
 
 export default function ProductScreen(props) {
+  const router = useRouter();
+  const { state, dispatch } = useContext(Store);
   const { product } = props;
   const classes = useStyles();
   if (!product) {
-    return <div>Product is not found</div>;
+    return <div>Product Not Found</div>;
   }
+  const addToCartHandler = async () => {
+    const existItem = state.cart.cartItems.find((x) => x._id === product._id);
+    const quantity = existItem ? existItem.quantity + 1 : 1;
+    const { data } = await axios.get(`/api/products/${product._id}`);
+    if (data.countInStock < quantity) {
+      window.alert('Sorry. Product is out of stock');
+      return;
+    }
+    dispatch({ type: 'CART_ADD_ITEM', payload: { ...product, quantity } });
+    router.push('/cart');
+  };
+
   return (
-    <Layout tittle={product.name} description={product.description}>
+    <Layout title={product.name} description={product.description}>
       <div className={classes.section}>
         <NextLink href="/" passHref>
-          <Typography>
-            <Link>Back to Products</Link>
-          </Typography>
+          <Link>
+            <Typography>back to products</Typography>
+          </Link>
         </NextLink>
       </div>
-
       <Grid container spacing={1}>
         <Grid item md={6} xs={12}>
           <Image
@@ -39,7 +55,7 @@ export default function ProductScreen(props) {
             width={640}
             height={640}
             layout="responsive"
-          />
+          ></Image>
         </Grid>
         <Grid item md={3} xs={12}>
           <List>
@@ -49,23 +65,22 @@ export default function ProductScreen(props) {
               </Typography>
             </ListItem>
             <ListItem>
-              <Typography>Category: {product.catagory}</Typography>
+              <Typography>Category: {product.category}</Typography>
             </ListItem>
             <ListItem>
               <Typography>Brand: {product.brand}</Typography>
             </ListItem>
             <ListItem>
               <Typography>
-                Rating: {product.rating} stars ({product.numReviews})
+                Rating: {product.rating} stars ({product.numReviews} reviews)
               </Typography>
             </ListItem>
             <ListItem>
-              <Typography>Description: {product.description}</Typography>
+              <Typography> Description: {product.description}</Typography>
             </ListItem>
           </List>
         </Grid>
-
-        <Grid item md={3} sx={6}>
+        <Grid item md={3} xs={12}>
           <Card>
             <List>
               <ListItem>
@@ -74,7 +89,7 @@ export default function ProductScreen(props) {
                     <Typography>Price</Typography>
                   </Grid>
                   <Grid item xs={6}>
-                    <Typography>৳{product.price}</Typography>
+                    <Typography>${product.price}</Typography>
                   </Grid>
                 </Grid>
               </ListItem>
@@ -85,14 +100,19 @@ export default function ProductScreen(props) {
                   </Grid>
                   <Grid item xs={6}>
                     <Typography>
-                      {product.countInStock > 0 ? 'In Stock' : 'Out of Stock'}
+                      {product.countInStock > 0 ? 'In stock' : 'Unavailable'}
                     </Typography>
                   </Grid>
                 </Grid>
               </ListItem>
               <ListItem>
-                <Button fullWidth variant="contained" color="primary">
-                  Add to Cart
+                <Button
+                  fullWidth
+                  variant="contained"
+                  color="primary"
+                  onClick={addToCartHandler}
+                >
+                  Add to cart
                 </Button>
               </ListItem>
             </List>
@@ -116,3 +136,137 @@ export async function getServerSideProps(context) {
     },
   };
 }
+
+// import React, { useContext } from 'react';
+// import Image from 'next/image';
+// import NextLink from 'next/link';
+// import {
+//   Grid,
+//   Link,
+//   List,
+//   ListItem,
+//   Typography,
+//   Card,
+//   Button,
+// } from '@mui/material';
+// import Layout from '../../components/Layout';
+// import useStyles from '../../utils/styles';
+// import db from '../../utils/db';
+// import Store from '../../utils/Store';
+// import Product from '../../models/Product';
+
+// export default function ProductScreen(props) {
+//   const { dispatch } = useContext(Store);
+//   const { product } = props;
+//   const classes = useStyles();
+//   if (!product) {
+//     return <div>Product is not found</div>;
+//   }
+//   const addToCartHandler = async () => {
+//     const data = await axios.get(`api/products/${product._id}`);
+//     if (data.countInStock <= 0) {
+//       window.alert('Sorry, Product Out of stock');
+//       return;
+//     }
+//     dispatch({ type: 'CART_ADD_ITEM', payload: { ...product, quantity: 1 } });
+//   };
+//   return (
+//     <Layout tittle={product.name} description={product.description}>
+//       <div className={classes.section}>
+//         <NextLink href="/" passHref>
+//           <Typography>
+//             <Link>Back to Products</Link>
+//           </Typography>
+//         </NextLink>
+//       </div>
+
+//       <Grid container spacing={1}>
+//         <Grid item md={6} xs={12}>
+//           <Image
+//             src={product.image}
+//             alt={product.name}
+//             width={640}
+//             height={640}
+//             layout="responsive"
+//           />
+//         </Grid>
+//         <Grid item md={3} xs={12}>
+//           <List>
+//             <ListItem>
+//               <Typography component="h1" variant="h1">
+//                 {product.name}
+//               </Typography>
+//             </ListItem>
+//             <ListItem>
+//               <Typography>Category: {product.catagory}</Typography>
+//             </ListItem>
+//             <ListItem>
+//               <Typography>Brand: {product.brand}</Typography>
+//             </ListItem>
+//             <ListItem>
+//               <Typography>
+//                 Rating: {product.rating} stars ({product.numReviews})
+//               </Typography>
+//             </ListItem>
+//             <ListItem>
+//               <Typography>Description: {product.description}</Typography>
+//             </ListItem>
+//           </List>
+//         </Grid>
+
+//         <Grid item md={3} sx={6}>
+//           <Card>
+//             <List>
+//               <ListItem>
+//                 <Grid container>
+//                   <Grid item xs={6}>
+//                     <Typography>Price</Typography>
+//                   </Grid>
+//                   <Grid item xs={6}>
+//                     <Typography>৳{product.price}</Typography>
+//                   </Grid>
+//                 </Grid>
+//               </ListItem>
+//               <ListItem>
+//                 <Grid container>
+//                   <Grid item xs={6}>
+//                     <Typography>Status</Typography>
+//                   </Grid>
+//                   <Grid item xs={6}>
+//                     <Typography>
+//                       {product.countInStock > 0 ? 'In Stock' : 'Out of Stock'}
+//                     </Typography>
+//                   </Grid>
+//                 </Grid>
+//               </ListItem>
+//               <ListItem>
+//                 <Button
+//                   fullWidth
+//                   variant="contained"
+//                   color="primary"
+//                   onClick={addToCartHandler}
+//                 >
+//                   Add to Cart
+//                 </Button>
+//               </ListItem>
+//             </List>
+//           </Card>
+//         </Grid>
+//       </Grid>
+//     </Layout>
+//   );
+// }
+
+// export async function getServerSideProps(context) {
+//   const { params } = context;
+//   const { slug } = params;
+
+//   await db.connect();
+//   const product = await Product.findOne({ slug }).lean();
+//   await db.disconnect();
+//   return {
+//     props: {
+//       product: db.convertDocToObj(product),
+//     },
+//   };
+// }
